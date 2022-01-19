b32804fb5b460db47511d1650ace6f46558ff9d3
Mar Fernndez <fernanda.fernandez@edify.cr>
2022-01-19 15:39:16 -0600

    Test N+1 -  Test Cases Execution

--- wdio-github.conf.js
+++ wdio-github.conf.js
@@ -10,7 +10,7 @@ exports.config = {
             acceptInsecureCerts: true,
             // We need to extends some Chrome flags in order to tell Chrome to run headless
             'goog:chromeOptions': {
-                args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage'],
+                args: ['--headless', "--no-sandbox", '--disable-gpu', '--disable-dev-shm-usage'],
             },
         },
     ],
