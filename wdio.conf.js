aa7a06195fb7d4fab0100d07ae562adb61b1541c
Mar Fernndez <fernanda.fernandez@edify.cr>
2022-01-19 15:46:18 -0600

    Test N+1 -  Test Cases Execution

--- wdio.conf.js
+++ wdio.conf.js
@@ -50,22 +50,17 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-    
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 1,
-        //
         browserName: 'chrome',
         acceptInsecureCerts: true,
-        chromeOptions: {
-            args: ['--headless', "--no-sandbox", '--disable-gpu', '--disable-dev-shm-usage'],
-        },
+        'goog:chromeOptions': {
+            args: ['--headless', '--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage --window-size=1920,1080', '--disable-translate', '--disable-extensions', '--disable-background-networking', '--disable-sync', '--disable-default-apps', '--mute-audio', '--no-first-run', '--disable-prompt-on-repost']
+            },
+    }],
+
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
-    }],
     //
     // ===================
     // Test Configurations
